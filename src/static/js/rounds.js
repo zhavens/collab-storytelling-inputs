83a7c1118cf4e61c9dfc923a660371798d4fd1e8
Raquel Thiessen <raquelthiessen@gmail.com>
2022-03-25 14:45:26 -0500

    Add pseudonym form to start page

--- src/static/js/rounds.js
+++ src/static/js/rounds.js
@@ -3,10 +3,33 @@
  * ---------------*/
 
 var maxRounds = 4;
-var startText = document.getElementById('start_text');
 var endRoundBtn = document.querySelector('#end_round_btn');
 
+function setPseudonym() {
+    pseudonymInput = document.getElementById("code_name").value;
+    if(pseudonymInput != ""){
+        localStorage.setItem("pseudonym", pseudonymInput);
+        console.log("Pseudonym: " + pseudonymInput);
+        showStartButton();
+    }
+}
+
+function showNameForm() {
+    var nameForm = document.getElementById('name_form');
+    var nextBtn = document.getElementById('next_btn');
+    nameForm.style.display = "block";
+    nextBtn.style.display = "none";
+}
+
+function showStartButton() {
+    var nameForm = document.getElementById('name_form');
+    var nextBtn = document.getElementById('next_btn');
+    nameForm.style.display = "none";
+    nextBtn.style.display = "block";
+}
+
 ready = function getStartText() {
+    var startText = document.getElementById('start_text');
     if (startText == null) {
         return;
     }
@@ -14,16 +37,22 @@ ready = function getStartText() {
     var round = localStorage.getItem("round");
     if (round == null || parseInt(round) > maxRounds) {
         localStorage.setItem("round", 1);
+        localStorage.setItem("pseudonym", null);
     }
 
     round = parseInt(localStorage.getItem("round"));
-
     if (round == 1) {
         startText.textContent = "Collaborative Storytelling";
     } else {
         startText.textContent = "ROUND " + round;
     }
     console.log('About to Start Round ' + round);
+
+    if(localStorage.getItem("pseudonym") != "null") {
+        showStartButton();
+    } else {
+        showNameForm();
+    }
 }
 document.addEventListener("DOMContentLoaded", ready);
 
@@ -34,6 +63,6 @@ function updateRound() {
 }
 
 function startRound() {
-    console.log("Round")
+    var round = parseInt(localStorage.getItem("round"));
     console.log("Round " + round + " : " + new Date().getTime());
 }
