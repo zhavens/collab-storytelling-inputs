6e140946ce07e8aeb67262340b4cc8fe7c7a2b64
Raquel Thiessen <raquelthiessen@gmail.com>
2022-04-04 15:08:13 -0500

    Add donwload functionality

--- src/static/js/output.js
+++ src/static/js/output.js
@@ -2,6 +2,7 @@ var maxRounds = 4;
 var round = localStorage.getItem("round");
 var finalStory = document.getElementById("final_story");
 var image = document.getElementById("output_image");
+var shareBtn = document.getElementById("share_btn");
 
 if(round == maxRounds) {
     // set all rounds of story images
@@ -20,8 +21,26 @@ window.onload = function () {
     if(round == 4) {
         image.style.display = "none";
         finalStory.style.display = "block";
+        shareBtn.style.display = "block";
     } else {
         image.style.display = "block";
         finalStory.style.display = "none";
+        shareBtn.style.display = "none";
     }
+}
+
+function shareStory() {
+    // download all images
+    for(var i = 1; i <= maxRounds; i++){
+        var imageName = localStorage.getItem("pseudonym") + i;
+        download(localStorage.getItem(imageName));
+    }
+}
+
+function download(url){
+    var link = document.createElement('a');
+    var var_name = localStorage.getItem("pseudonym") + localStorage.getItem("round");
+    link.download = var_name + '.png';
+    link.href = url;
+    link.click();
 }
