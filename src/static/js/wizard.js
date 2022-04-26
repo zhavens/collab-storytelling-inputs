6491ec2a4f73c0e28b9f1b224c6be910e915ed22
zhavens <zhavens@hotmail.com>
2022-04-25 23:47:35 -0500

    Supporting simultaneous wizarding.

--- src/static/js/wizard.js
+++ src/static/js/wizard.js
@@ -1,38 +1,35 @@
 import categories from "./categories.js";
 
-var currUser = "";
-
-function appendCategory(category) {
-    $('#newCats').append($(document.createElement('a'))
-        .addClass("list-group-item list-group-item-action newCat")
-        .attr('href', '#')
-        .text(category)
-        .click(event => {
-            event.target.remove()
-        }));
-};
-
-function addConsoleLog(msg, classes = null) {
-    var p = $(document.createElement("p")).html(msg).addClass(classes);
-    $('#output').append(p);
-    p[0].scrollIntoView(false);
+function setUser(panel, user) {
+    panel[0].dataset.user = user;
 }
 
-function connectConsole() {
-    var socket = new WebSocket('wss://zhavens.com/hai/wizard/' + encodeURIComponent(currUser));
+function getUser(panel) {
+    return panel[0].dataset.user;
+}
+
+function addConsoleLog($console, msg, classes = null) {
+    var p = $(document.createElement("p")).html(msg).addClass(classes);
+    $console.append(p);
+    $console[0].scrollTop = $console[0].scrollHeight;
+    // p[0].scrollIntoView(false);
+}
+
+function connectConsole($console, user) {
+    var socket = new WebSocket('wss://zhavens.com/hai/wizard/' + encodeURIComponent(user));
 
     socket.addEventListener('open', event => {
         $('#connIcon').removeClass("fail bi-x-circle-fill")
             .addClass("success bi-check-circle-fill");
         var msg = new Date().toISOString() + ": Connected.\n"
-        addConsoleLog(msg, "success");
+        addConsoleLog($console, msg, "success");
     });
 
     socket.addEventListener('message', event => {
         var data = JSON.parse(event.data);
         if (data.type == "log") {
             data.log.timestamp = new Date(data.log.timestamp);
-            addConsoleLog(data.log.timestamp.toISOString() + ': ' + data.log.msg + '\n')
+            addConsoleLog($console, data.log.timestamp.toISOString() + ': ' + data.log.msg + '\n')
         }
     });
 
@@ -40,55 +37,20 @@ function connectConsole() {
         $('#connIcon').removeClass("success bi-check-circle-fill")
             .addClass("fail bi-x-circle-fill");
         var msg = new Date.toISOString() + ": Error - " + event + "\n";
-        addConsoleLog(msg, "fail");
+        addConsoleLog($console, msg, "fail");
     });
 
     socket.addEventListener('close', event => {
         $('#connIcon').removeClass("success bi-check-circle-fill")
             .addClass("fail bi-x-circle-fill");
         var msg = new Date.toISOString() + ": Disonnected.\n"
-        addConsoleLog(msg, "fail");
+        addConsoleLog($console, msg, "fail");
     });
 };
 
-function fetchCategories() {
-    const categoryUrl = "https://zhavens.com/hai/categories/" + encodeURIComponent(currUser);
-    $.getJSON(categoryUrl, data => {
-        $(".newCat").remove();
-        for (var category of data.categories) {
-            appendCategory(category);
-        }
-    });
-}
-
-$('#userForm').on('submit', (event) => {
-    event.preventDefault();
-
-    currUser = $('#userInput').val();
-    connectConsole();
-    fetchCategories();
-});
-
-$('#connectBtn').on('click', connectConsole);
-
-
-$('#addCatForm').on('submit', event => {
-    event.preventDefault();
-    appendCategory($('#category').val().toLowerCase());
-    $('#category').val("");
-})
-
-$('#newCatForm').on('submit', async event => {
-    event.preventDefault();
-
-    var newCategories = [];
-
-    $('.newCat').each((idx, el) => {
-        newCategories.push(el.text);
-    });
-
+async function postCategories(user, newCategories, $console) {
     var url = "https://zhavens.com/hai/categories/"
-    url += encodeURIComponent(currUser);
+    url += encodeURIComponent(user);
 
     await $.ajax(url,
         {
@@ -97,8 +59,70 @@ $('#newCatForm').on('submit', async event => {
             data: JSON.stringify({ "categories": newCategories }),
         }).done(() => {
             var msg = new Date().toISOString() + ": Updated categories - " + newCategories + ".\n";
-            addConsoleLog(msg)
+            if ($console)
+                addConsoleLog($console, msg)
         }).catch(console.error);
+}
+
+function appendCategory($list, category) {
+    $list.append($(document.createElement('a'))
+        .addClass("list-group-item list-group-item-action new-cat")
+        .attr('href', '#')
+        .text(category)
+        .click(event => {
+            event.target.remove()
+        }));
+};
+
+function populateCategories($list, user) {
+    const categoryUrl = "https://zhavens.com/hai/categories/" + encodeURIComponent(user);
+    $.getJSON(categoryUrl, data => {
+        $list.find(".new-cat").remove();
+        if (data && data.categories) {
+            for (var category of data.categories) {
+                appendCategory($list, category);
+            }
+        }
+    });
+}
+
+$('form.user-form').on('submit', (event) => {
+    event.preventDefault();
+    var panel = $(event.target).closest(".wizard-panel");
+
+    var user = panel.find('.user-input').val();
+    setUser(panel, user);
+    connectConsole(panel.find(".console"), user);
+    populateCategories(panel.find(".cat-list"), user);
+});
+
+$('button.connect').on('click', event => {
+    var panel = $(event.target).closest('.wizard-panel');
+
+    connectConsole(panel.find('console'), getUser(panel));
+});
+
+
+$('form.add-cat-form').on('submit', event => {
+    event.preventDefault();
+    var panel = $(event.target).closest('.wizard-panel');
+    var input = panel.find('input.category-input');
+    appendCategory(panel.find(".cat-list"),
+        input.val().toLowerCase());
+    input.val("");
+})
+
+$('form.post-cat-form').on('submit', async event => {
+    event.preventDefault();
+
+    var panel = $(event.target).closest('.wizard-panel');
+    var newCategories = [];
+
+    panel.find('.new-cat').each((idx, el) => {
+        newCategories.push(el.text);
+    });
+
+    postCategories(getUser(panel), newCategories, panel.find(".console"));
 });
 
 // https://codepen.io/tomfarina/pen/wZyPeZ
@@ -115,7 +139,7 @@ var substringMatcher = function (strs) {
     };
 };
 
-$('#category').typeahead({
+$('.category-input').typeahead({
     highlight: false,
     hint: false,
     minlength: 1
@@ -131,7 +155,8 @@ $('#category').typeahead({
     });
 
 
-$('#category').on("typeahead:selected", function (eventObject, suggestion, name) {
-    $('#category').val(suggestion);
-    $('#addCatForm').submit();
+$('.category-input').on("typeahead:selected", function (event, suggestion, name) {
+    var panel = $(event.target).closest('.wizard-panel');
+    panel.find('.category-input').val(suggestion);
+    panel.find('.add-cat-form').submit();
 });
