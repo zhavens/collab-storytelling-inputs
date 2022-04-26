ac1f6275d9ea32b11664bd293080d83905971e4a
zhavens <zhavens@hotmail.com>
2022-04-26 00:11:14 -0500

    More debug fixes.

--- src/routes/images.js
+++ src/routes/images.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var debug = require('debug')('images')
 
 var mongo = require('mongodb');
 
@@ -9,19 +10,19 @@ const client = new mongo.MongoClient(uri);
 /* POST users listing. */
 router.post('/:user', async function (req, res, next) {
     if (!req.params.user) {
-        console.log('Error: missing field "user".');
+        debug('Error: missing field "user".');
         res.status(400).send('Error: missing field "user".');
     } else if (!req.body.image) {
-        console.log('Error: missing field "image".');
+        debug('Error: missing field "image".');
         res.status(400).send('Error: missing field "image".');
     } else if (!req.body.inputVersion) {
-        console.log('Error: missing field "inputVersion".');
+        debug('Error: missing field "inputVersion".');
         res.status(400).send('Error: missing field "inputVersion".');
-    }  else if (!req.body.outputVersion) {
-        console.log('Error: missing field "outputVersion".');
+    } else if (!req.body.outputVersion) {
+        debug('Error: missing field "outputVersion".');
         res.status(400).send('Error: missing field "outputVersion".');
-    }  else if (!req.body.round) {
-        console.log('Error: missing field "round".');
+    } else if (!req.body.round) {
+        debug('Error: missing field "round".');
         res.status(400).send('Error: missing field "round".');
     } else {
         try {
@@ -38,7 +39,7 @@ router.post('/:user', async function (req, res, next) {
                 timestamp: mongo.Timestamp(),
             };
             const result = await inter.insertOne(doc);
-            console.log(`A document was inserted with the _id: ${result.insertedId}`);
+            debug(`A document was inserted with the _id: ${result.insertedId}`);
             res.send('Success.');
         } catch {
             res.status(500).send('Error posting image DB.');
@@ -51,10 +52,10 @@ router.post('/:user', async function (req, res, next) {
 /* GET users listing. */
 router.get('/:user', async function (req, res, next) {
     if (!req.params.user) {
-        console.log('Error: missing field "user".');
+        debug('Error: missing field "user".');
         res.status(400).send('Error: missing field "user".');
     } else if (!req.body.inputVersion) {
-        console.log('Error: missing field "inputVersion".');
+        debug('Error: missing field "inputVersion".');
         res.status(400).send('Error: missing field "inputVersion".');
     } else {
         try {
